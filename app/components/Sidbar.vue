0fe94a72f35fd4eb40898f75f4bebd3a72ef6657
Jiangshuuu <globelex65@gmail.com>
2025-01-11 10:04:33 +0800

    upd

--- app/components/Sidbar.vue
+++ app/components/Sidbar.vue
@@ -9,7 +9,6 @@
             class="w-full justify-start"
             @click="navigateTo('/')"
           >
-            <HomeIcon class="mr-2 h-4 w-4" />
             首頁
           </Button>
           <Button
@@ -17,7 +16,6 @@
             class="w-full justify-start"
             @click="navigateTo('/login')"
           >
-            <LayoutDashboardIcon class="mr-2 h-4 w-4" />
             登入相關
           </Button>
         </div>
@@ -30,7 +28,6 @@
             class="w-full justify-start"
             @click="navigateTo('/settings')"
           >
-            <SettingsIcon class="mr-2 h-4 w-4" />
             系統設定
           </Button>
           <Button
@@ -38,7 +35,6 @@
             class="w-full justify-start"
             @click="navigateTo('/profile')"
           >
-            <UserIcon class="mr-2 h-4 w-4" />
             個人資料
           </Button>
         </div>
--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,6 +1,25 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { fileURLToPath } from 'node:url'
+import { dirname, join } from 'node:path'
+
+const currentDir = dirname(fileURLToPath(import.meta.url))
+const uiDir = join(currentDir, '../ui')  // 指向 ui 專案目錄
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
-  extends: ['../layer', '../ui'],
+  extends: '../ui',
+
+  // 指定 shadcn 組件目錄為 ui 專案的目錄
+  shadcn: {
+    prefix: '',
+    componentDir: join(uiDir, 'components/ui')
+  },
+
+  // 添加 ui 專案的組件目錄
+  components: [
+    {
+      path: join(uiDir, 'components/ui'),
+      extensions: ['.vue']
+    }
+  ],
+
+  compatibilityDate: '2025-01-11'
 })
